3b3476ecdbf530069bbe556fd59fc59b9f157d74
Queen Nnakwue <qnnakwue@gmail.com>
2021-08-24 12:04:32 +0100

    adding more features

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,19 +2,42 @@ import React from 'react';
 import "../styles/Home.css";
 import Product from './Product' 
 
+
 function Home() {
     return (
 			<div classname='home'>
 				<div className='home__container'>
-					{/* <img
-						src='https://images.unsplash.com/photo-1521193089946-7aa29d1fe776?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80'
-						className='home__image'
-						alt='home_image'
-					/> */}
 					<div className='home__row'>
-						<Product />
+						<Product 
+						title='The lean startup'
+						price={29.99}
+						image='https://images-na.ssl-images-amazon.com/images/I/91cwOSS4sDL.jpg'
+						/>
+
+						<Product 
+						title="KenWood KMIX"
+						price={50.99}
+						image='https://www.kenwoodworld.com/WebImage/Global/Product%20images/Kmix%20products/KMX754RD/KMX754RD_Picasso-KM-Glass-Bowl-BMK_V2_800x600.png'
+						
+						/>
 					</div>
 				</div>
+					<div className='home__row'>
+							<Product />
+							<Product />
+							<Product />
+					</div>
+
+					<div className='home__row'>
+							<Product />
+							<Product />
+							<Product />
+							<Product />
+					</div>
+
+					<div className='home__row'>
+							<Product />	
+					</div>	
 			</div>
 		);
 }
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,28 +1,25 @@
 import React from 'react';
 import "../styles/Product.css";
 
-function Product() {
+function Product({title, price, image}) {
     return (
 			<div className='product'>
 				<div className='product__info'>
-					<p>
-						The lean startup: How Constant Innovation <br /> Creates rapidly
-						Successful Businesses <br /> Paperback
-					</p>
+					<p>{title}</p>
 					<p className='product__price'>
 						<small>$</small>
-						<price>19.22</price>
+						<price>{price}</price>
 					</p>
 				</div>
 				<div className='product__rating'>
 					<img
 						src='https://cdn.pixabay.com/photo/2016/03/31/17/33/icons-1293736_960_720.png'
 						className='product__star'
-						alt='star'
+						alt='star' 
 					/>
 				</div>
 				<img
-					src='https://images-na.ssl-images-amazon.com/images/I/91cwOSS4sDL.jpg'
+					src={image}
 					className='product__image'
 					alt=''
                     />
